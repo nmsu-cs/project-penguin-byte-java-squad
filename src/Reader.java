222e96fe0dcaf6541d25bf9625c048c5008534bd
Jacob Yanez <jakey17@nmsu.edu>
2024-03-21 16:17:12 -0600

    added Reader class to read in an ingredient list

---
+++ src/Reader.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class Reader {
+
+    public static ArrayList<String> readList(String list) throws IOException {
+        String filePath = new File("").getAbsolutePath();
+        String modifiedPath = filePath.concat(list);
+        ArrayList<String> shoppingList = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new FileReader(modifiedPath));
+
+        String line;
+        while((line = br.readLine()) != null) {
+            shoppingList.add(line);
+        }
+        return shoppingList;
+    }
+
+    public static void main(String[] args) throws IOException {
+        ArrayList<String> ingredients = Reader.readList("/ingredients.txt");
+        System.out.println(ingredients);
+    }
+}
+
+
