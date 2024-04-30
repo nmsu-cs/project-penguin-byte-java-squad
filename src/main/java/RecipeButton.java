b331892eb61c6b71b17524fcf9dee86173f81731
Brady-Scaggari <scaggari@nmsu.edu>
2024-04-30 17:21:57 -0600

    cleaner recipe details look

--- src/main/java/RecipeButton.java
+++ src/main/java/RecipeButton.java
@@ -5,8 +5,11 @@ import com.formdev.flatlaf.themes.FlatMacDarkLaf;
 import com.formdev.flatlaf.fonts.roboto.FlatRobotoFont;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 
 public class RecipeButton extends JButton {
     private Recipe recipe = null;
@@ -83,6 +86,44 @@ public class RecipeButton extends JButton {
 //        nFrame.pack();
         nFrame.setLocationRelativeTo(null);
 
+        JPanel headerPanel = new JPanel();
+        headerPanel.setLayout(new MigLayout("ins 0","[]","[]3[]"));
+
+        JPanel titleLabel = new JPanel();
+        JLabel titleArea = new JLabel("what the heck");
+        titleLabel.setLayout(new MigLayout("wrap","[50%, grow][]","[][]"));
+        titleArea.setText("<html><h2 style='max-width: 300px;'>"+rp.getTitle()+"</h2></html>");
+
+        titleLabel.add(titleArea);
+        headerPanel.add(titleLabel);
+        titleLabel.setVisible(true);
+
+        //creating icon for recipe details
+        ImageIcon icon = new ImageIcon("images/"+rp.getTitle()+".png");
+        Image image = icon.getImage();
+        Image image1 = image.getScaledInstance(150, 125, Image.SCALE_SMOOTH);
+        ImageIcon icon2 = new ImageIcon(image1);
+        JLabel recipeLabel = new JLabel(icon2);
+        headerPanel.add(recipeLabel, BorderLayout.CENTER);
+
+
+        //add button for recipe url
+        JButton linkButton = new JButton("Recipe Website");
+        linkButton.setFont(new Font(linkButton.getFont().getName(), Font.BOLD, 24));
+        linkButton.setSize(new Dimension(20,20));
+
+
+        linkButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try{
+                    Desktop.getDesktop().browse(new URI(rp.getLink()));
+                }   catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
         JTextArea recipeDetails = new JTextArea();
         String [] instructions = rp.getInstructions();
         String [] ingredients = rp.getIngredients();
@@ -105,11 +146,17 @@ public class RecipeButton extends JButton {
         ins = ins.replaceAll("\"", "");
         ins = ins.replaceAll("\\]", "");
         ins = ins.replaceAll("\\[", "");
-        recipeDetails.setText("Title: " + rp.getTitle() + "\nIngredients: \n" + ing
-                + "Instructions: \n" + ins +
-                "Link: " + rp.getLink() + "\n");
+        recipeDetails.setText("Ingredients:" + "\n\n"
+                + ing + "\n" + "Instructions:" + " \n\n" + ins);
+        recipeDetails.setLineWrap(true);
+        recipeDetails.setWrapStyleWord(true);
+        recipeDetails.setFont(new Font(recipeDetails.getFont().getName(), Font.BOLD, 16));
+        recipeDetails.setEditable(false);
         JScrollPane detailsScroll = new JScrollPane(recipeDetails);
         nFrame.add(detailsScroll);
+        nFrame.add(linkButton, BorderLayout.SOUTH);
+        nFrame.add(headerPanel, BorderLayout.NORTH);
+        nFrame.setIconImage(image1);
         nFrame.setVisible(true);
     }
 }
